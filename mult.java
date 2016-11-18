d73121bec9efdcc4147f56a6f5a0bf6e802f472b
LelitoPT <cenasmaradaslelo@gmail.com>
2016-11-18 09:48:26 +0000

    no message

--- mult.java
+++ mult.java
@@ -12,12 +12,12 @@ public class Mult {
     System.out.print("m ? ");
     String m2 = in.nextLine();
     int int_m2 = Integer.parseInt(m2);
-// mudar nome de last_n
-    int last_n = m2.length();
 
-    int [] m2_ints = new int[last_n];
+    int m2_len = m2.length();
+
+    int [] m2_ints = new int[m2_len];
 // separar string em int e guardar em array
-    int z = last_n - 1;
+    int z = m2_len - 1;
     int z2 = 0;
     do {
       int tmp = Character.getNumericValue(m2.charAt(z));
@@ -28,13 +28,13 @@ public class Mult {
     //System.out.println(Arrays.toString(m2_ints));
     System.out.println("M = " + m1 + ", m = " + m2);
 
-    int [] tmp_cal = new int [last_n];
+    int [] tmp_cal = new int [m2_len];
 // calculos auxiliares para array
     int a = 0;
     do {
       tmp_cal[a] = int_m1 * m2_ints[a];
       a++;
-    } while (a < last_n);
+    } while (a < m2_len);
     //System.out.println(Arrays.toString(tmp_cal));
 /*
 P0 = M x m0 = 25811 x 5 = 129055
@@ -42,7 +42,7 @@ P1 = M x m1 = 25811 x 2 = 51622
 P2 = M x m2 = 25811 x 3 = 77433
 */
     int i = 0;
-    int i2 = last_n - 1;
+    int i2 = m2_len - 1;
     do {
       System.out.println(
         "P" + i + " = M x m" + i +  " = " +
@@ -51,7 +51,7 @@ P2 = M x m2 = 25811 x 3 = 77433
       );
       i++;
       i2--;
-    } while (i < last_n);
+    } while (i < m2_len);
 /*
 M x n = P0 + P1 x 10 + P2 x 100
 */
@@ -64,11 +64,11 @@ M x n = P0 + P1 x 10 + P2 x 100
       );
       tmp_cal[x] *= (int) Math.pow(10,x);
 
-      if (x != last_n - 1) {
+      if (x != m2_len - 1) {
         System.out.print(" ");
       }
       x++;
-    } while (x < last_n);
+    } while (x < m2_len);
 /*
 M x n = 129055 + 516220 + 7743300 = 8388575
 */
@@ -81,13 +81,13 @@ M x n = 129055 + 516220 + 7743300 = 8388575
       System.out.print(tmp_cal[y]);
       result += tmp_cal[y];
       //.out.println(result);
-      if (y != last_n - 1) {
+      if (y != m2_len - 1) {
         System.out.print(" + ");
       }
       else {
         System.out.print(" = " + result);
       }
       y++;
-    } while (y < last_n);
+    } while (y < m2_len);
   }
 }
